6b7a11dcdb3afea129e48631c83905826b0b5a24
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-05 14:02:31 -0400

    feat(conditional-probability): create module

--- src/features/probability/probability.module.ts
+++ src/features/probability/probability.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { ConditionalProbabilityModule } from './conditional-probability';
 import { JointProbabilityModule } from './joint-probability';
 
 @Module({
-  imports: [JointProbabilityModule],
-  exports: [JointProbabilityModule],
+  imports: [JointProbabilityModule, ConditionalProbabilityModule],
+  exports: [JointProbabilityModule, ConditionalProbabilityModule],
 })
 export class ProbabilityModule {}
