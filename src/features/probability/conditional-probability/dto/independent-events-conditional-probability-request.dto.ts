677242557e9919f7fd59f1cef515523d9646f86b
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-05 16:29:16 -0400

    fix(conditional-probability): add array and empty object constraints to independent events dto

--- src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
+++ src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
@@ -1,9 +1,11 @@
 import { ProbabilisticEventDto } from '@features/probability/shared';
-import { ValidateNested } from 'class-validator';
 import { Type } from 'class-transformer';
+import { IsArray, IsNotEmptyObject, ValidateNested } from 'class-validator';
 
 export class IndependentEventsConditionalProbabilityRequestDto {
   @ValidateNested({ each: true })
   @Type(() => ProbabilisticEventDto)
+  @IsArray()
+  @IsNotEmptyObject()
   events: ProbabilisticEventDto[];
 }
