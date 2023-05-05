921f7a19baa21f94d62d238446c55eba1fb5b1b8
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-05 15:39:15 -0400

    fix(conditional-probability): fix validation of each event probability

--- src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
+++ src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
@@ -1,7 +1,9 @@
 import { ProbabilisticEventDto } from '@features/probability/shared';
 import { ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
 
 export class IndependentEventsConditionalProbabilityRequestDto {
   @ValidateNested({ each: true })
+  @Type(() => ProbabilisticEventDto)
   events: ProbabilisticEventDto[];
 }
