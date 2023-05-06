94d10d1e1f1c193529335a1debe8698fdb2aaf9c
Cristóbal Gajardo Vera <cristobalgajardo.v@gmail.com>
2023-05-06 01:45:13 -0400

    fix(conditional-probability): remove is empty object validation for independent events

--- src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
+++ src/features/probability/conditional-probability/dto/independent-events-conditional-probability-request.dto.ts
@@ -1,11 +1,10 @@
 import { ProbabilisticEventDto } from '@features/probability/shared';
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmptyObject, ValidateNested } from 'class-validator';
+import { IsArray, ValidateNested } from 'class-validator';
 
 export class IndependentEventsConditionalProbabilityRequestDto {
   @ValidateNested({ each: true })
   @Type(() => ProbabilisticEventDto)
   @IsArray()
-  @IsNotEmptyObject()
   events: ProbabilisticEventDto[];
 }
